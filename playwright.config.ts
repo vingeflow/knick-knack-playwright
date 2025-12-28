ccd5976058d87ee734dcb5384b2300334c8cfa2f
Maksim <maksimljaussov@gmail.com>
2025-12-28 23:08:38 +0200

    Add more tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ const isStaging = process.env.ENV === 'staging'
 
 export default defineConfig({
   testDir: './src/tests',
-  timeout: 10000,
+  timeout: 30000,
   retries: 1,
   maxFailures: 5,
   reporter: [
